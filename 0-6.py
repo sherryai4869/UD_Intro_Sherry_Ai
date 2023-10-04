acdb42973957282f7de5b34d6c3b90a2328f2816
sherryai4869 <146765110+sherryai4869@users.noreply.github.com>
2023-10-04 08:14:36 +0800

    Add files via upload

---
+++ 0-6.py
@@ -0,0 +1,24 @@
+print("hello world!")
+myName="Sherry Ai"
+myEmail="ucbvaix@ucl.ac.uk"
+myAge="23"
+print(myName+"\n"+myEmail+"\n"+myAge)
+print(0,178,-21,2938/49,436*9948,12**20)
+import math
+math.sin(200)
+math.cos(100)
+math.pi/4
+stringA = "Sherry"
+stringB = "Ai"
+phrase = stringA + ' ' + stringB
+
+print(phrase)
+n = 9
+
+if(n>0):
+    print("n is a positive number")
+elif(n<0):
+    print("n is a negative number")
+else:
+    print("n=0")
+
